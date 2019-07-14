3a3f79ce5349dc2fa6ba59778987fdb31da9c81b
mateusz.zbikowski <mateusz.zbikowski@gmail.com>
2019-07-14 14:27:56 +0200

    Second Day desc

--- src/main/scala/com/coding/problem/Problem2.scala
+++ src/main/scala/com/coding/problem/Problem2.scala
@@ -2,6 +2,20 @@ package com.coding.problem
 
 import scala.collection.mutable.ArrayBuffer
 
+/**
+  * 
+  * This problem was asked by Uber.
+  *
+  * Given an array of integers, return a new array such that
+  * each element at index i of the new array is the product
+  * of all the numbers in the original array except the one at i.
+  *
+  * For example, if our input was [1, 2, 3, 4, 5], the expected
+  * output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1],
+  * the expected output would be [2, 3, 6].
+  *
+  */
+
 object Problem2 extends App {
 
   val input = List(3,2,1)
